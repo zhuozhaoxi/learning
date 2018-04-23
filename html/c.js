3dee1a462e3f816de749d05a177f84d2cd3058c9
zhuozhaoxi <573741002@qq.com>
2018-04-23 20:10:54 +0800

    html mainfest
    html mainfest测试

---
+++ html/c.js
@@ -0,0 +1,4 @@
+function say(){
+    alert('我是在线的');
+}
+document.getElementById('box').innerHTML="在线状态";
---
+++ html/offline.js
@@ -0,0 +1,4 @@
+function say(){
+    alert('我是离线的');
+}
+document.getElementById('box').innerHTML="离线状态";
