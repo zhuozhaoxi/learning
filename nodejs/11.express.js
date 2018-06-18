1f49c3c3d0c448365d5c1a8e326a5714db23f415
zhaoxi <zhuozhaoxi@jd.com>
2018-06-18 15:31:55 +0800

    获取post参数

--- nodejs/11.express.js
+++ nodejs/11.express.js
@@ -11,10 +11,6 @@ app.get('/', function (req, res) {
 
 //  POST 请求
 app.post('/', function (req, res) {
-    //req.query;    // 获取URL的查询参数串
-    //req.params    // 获取路由的parameters
-
-    console.log("主页 POST 请求");
     res.send('Hello POST');
 });
 
@@ -31,11 +27,14 @@ app.get('/index.htm', function (req, res) {
 
 
 app.get('/process_get', function (req, res) {
+    // 从 req.query 中取出我们的 q 参数。
+    // 如果是 post 传来的 body 数据，则是在 req.body 里面，不过 express 默认不处理 body 中的信息，需要引入 https://github.com/expressjs/body-parser 这个中间件才会处理，这个后面会讲到。
     // 输出 JSON 格式
     var response = {
         "first_name":req.query.first_name,
         "last_name":req.query.last_name
     };
+    console.log(req.query);
     console.log(response);
     res.end(JSON.stringify(response));
 });
@@ -46,8 +45,24 @@ app.get('/user/:id', function (req, res) {
 });
 
 var bodyParser = require('body-parser');
-// 创建 application/x-www-form-urlencoded 编码解析
+// 创建 application/x-www-form-urlencoded 编码解析 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// app.use(bodyParser.json());  json解析
+
+
+// form-data 解析
+/**
+    var multipart = require('connect-multiparty');
+    var multipartMiddleware = multipart();
+    app.post('/formdata',multipartMiddleware, function (req, res) {
+        console.log(req.body);
+        res.send("post successfully!");
+    });
+    或者
+    app.use(multipartMiddleware);
+ **/
+
+// 仅在该接口使用这个中间件。
 app.post('/process_post', urlencodedParser, function (req, res) {
 
     // 输出 JSON 格式
@@ -55,6 +70,7 @@ app.post('/process_post', urlencodedParser, function (req, res) {
         "first_name":req.body.first_name,
         "last_name":req.body.last_name
     };
+    console.log(req.body);
     console.log(response);
     res.end(JSON.stringify(response));
 });
